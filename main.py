6709e96d56b7c593cbf477c82ffb85fa0d42f6c3
FrankChe <chexiaoyu@chexiaoyudeMacBook-Air.local>
2015-07-20 14:33:15 +0800

    Add the subsets process

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 '''
 top-k pattern mining
 '''
+import math
+import time
 
 def importData(name) :
 	# import data from name
 	try :
 		fo = open(name)
 		data = [lst.strip().split(',') for lst in fo.readlines()]
-		return [t[1:5] for t in data], [t[0] for t in data]
+		return [t[1:] for t in data], [t[0] for t in data]
 	except Exception, e :
 		print e
 	finally :
 		fo.close()
+
+
 # --- end of importData ---
 
 class Pattern :
@@ -19,6 +23,7 @@ class Pattern :
 	pattern = dict()
 
 	# methods
+
 	def __init__(self, *arr) :
 		for p, v in arr :
 			self.pattern[p] = v
@@ -59,6 +64,25 @@ class Pattern :
 				nage.append(dataset[i])
 		posifrq, nagefrd = self.frequency(posi), self.frequency(nage)
 
+
+		#Get all subsets of the set
+		store = []
+		new = []
+		print len(dataset[0])
+		total = 2**len(dataset[0])
+		num = 0;
+		while num < total:
+			new = bin(num)[2:]
+			store.append(new)
+			num += 1
+		print store[10]
+
+
+
+
+
+
+
 	def toString(self) :
 		s = ''
 		for i in self.pattern.keys() :
@@ -66,9 +90,15 @@ class Pattern :
 		return s
 
 
+start = time.clock()
+
 if __name__ == '__main__' :
 	dataset, label = importData('data.txt')
 	p1 = Pattern([0, 'x'], [1, 's'], [2, 'w'])
 	p1.disp()
 	print p1.globaldiff(dataset, label)
 	print p1.toString()
+	print p1.localdiff(dataset, label,[0,'x'])
+
+elapsed = (time.clock() - start)
+print "Time used:" ,elapsed," s"
